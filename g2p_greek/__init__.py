458bb0e44fffe2e7e432ad371231f851345442de
geoph9 <george.karakasides@gmail.com>
2020-03-12 17:50:31 +0200

    update init

--- g2p_greek/__init__.py
+++ g2p_greek/__init__.py
@@ -7,18 +7,10 @@ g2p-greek
 Grapheme to Phoneme conversion for greek.
 
 """
-
-from __future__ import print_function
-from __future__ import unicode_literals
-from __future__ import division
 import logging
 
 import g2p_greek.g2p_greek
 import g2p_greek.digits_to_words
-
-__title__ = 'g2p_greek'
-__version__ = '0.0.1'
-__author__ = 'George Karakasidis'
 
 log = logging.getLogger(__name__)
 log.addHandler(logging.NullHandler())
